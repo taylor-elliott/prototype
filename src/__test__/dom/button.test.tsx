5f768230e26f1fe074b5029a893ea5fa4ad47935
Taylor D. Elliott <taylor.doug.elliott@gmail.com>
2025-10-24 13:56:26 -0400

    Button test now resmbles current Button props

--- src/__test__/dom/button.test.tsx
+++ src/__test__/dom/button.test.tsx
@@ -2,6 +2,6 @@ import { render } from "@testing-library/react";
 import Button from "../../components/Button";
 
 test("renders button with label [button-test]", () => {
-    const { getByText } = render(<Button label="abc" />);
+    const { getByText } = render(<Button onClick={() => { }}>abc</Button>);
     expect(getByText("abc")).toBeInTheDocument();
 });
